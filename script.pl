e5337df46c782a3d26431012201a206aa523aa7c
Nathan Dautenhahn <daut@serenitix.io>
2025-05-26 09:08:25 -0500

    perl: have to have diamond operator option *starryeyes*
    Signed-off-by: Nathan Dautenhahn <daut@serenitix.io>

--- script.pl
+++ script.pl
@@ -19,16 +19,25 @@ my %data = (
     age  => 42,
 );
 
-# Function
+# Function to process a line
 sub process_line {
     my ($line) = @_;
     chomp $line;
-    return uc($line);  # uppercase transformation
+    return uc($line);  # example transformation
 }
 
-# File processing
+# --- Option 1: Explicit Filehandle ---
+print "--- Explicit Filehandle Method ---\n" if $verbose;
 open my $fh, '<', $input or die "Can't open $input: $!";
 while (my $line = <$fh>) {
     print process_line($line), "\n" if $verbose;
 }
 close $fh;
+
+# --- Option 2: Diamond Operator ---
+print "--- Diamond Operator Method ---\n" if $verbose;
+# Localize ARGV to our input file
+@ARGV = ($input);
+while (<>) {
+    print process_line($_), "\n" if $verbose;
+}
